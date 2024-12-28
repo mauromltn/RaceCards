6e7d353799efc27fe902e434f0298bc0b9edb08a
mauromltn <mauromtne@icloud.com>
2024-12-28 23:15:49 +0100

    Initial Commit

---
+++ RaceCards/RaceCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaceCardsApp.swift
+//  RaceCards
+//
+//  Created by Mauro Montané Meliá on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RaceCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
